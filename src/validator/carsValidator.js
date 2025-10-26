d12f3239bdf1fa23c6083d454b09731ee5c505a8
PixeIDark <kiki7951@naver.com>
2025-10-27 03:41:59 +0900

    feat(validator/cars): 중복 및 자동차 개수 검증 함수 추가
    - validateNoDuplicates: 자동차 이름 배열 내 중복 확인 및 에러 처리
    - validateCarCount: 자동차 배열 길이가 최소/최대 범위 내인지 검증

--- src/validator/carsValidator.js
+++ src/validator/carsValidator.js
@@ -1,4 +1,4 @@
-import { MAX_CAR_NAME_LENGTH, MIN_CAR_NAME_LENGTH } from "../constants/config.js";
+import {MAX_CAR_COUNT, MAX_CAR_NAME_LENGTH, MIN_CAR_COUNT, MIN_CAR_NAME_LENGTH} from "../constants/config.js";
 import { ERROR_MESSAGES } from "../constants/errorMessages.js";
 
 export const validateCarsNotEmpty = (trimmedCars) => {
@@ -13,16 +13,28 @@ export const validateCarNameMaxLength = (carName) => {
   if(carName.length > MAX_CAR_NAME_LENGTH) throw new Error(ERROR_MESSAGES.CAR_NAME_TOO_LONG);
 };
 
+export const validateNoDuplicates = (carArray) => {
+  const uniqueCars = [...new Set(carArray)];
+
+  if (uniqueCars.length !== carArray.length) throw new Error(ERROR_MESSAGES.CAR_NAME_DUPLICATED);
+};
+
+export const validateCarCount = (carArray) => {
+  if(carArray.length < MIN_CAR_COUNT || carArray.length > MAX_CAR_COUNT) throw new Error(ERROR_MESSAGES.CAR_COUNT_OUT_OF_RANGE);
+};
+
 export const validateCars = (cars) => {
   const trimmedCars = cars.trim();
-
   validateCarsNotEmpty(trimmedCars);
 
-  for(const car of trimmedCars.split(",")) {
-    const carName = car.trim();
+  const carArray = trimmedCars.split(",").map(car => car.trim());
+  carArray.forEach(carName => {
     validateCarNameMinLength(carName);
     validateCarNameMaxLength(carName);
-  }
+  });
 
-  return trimmedCars;
+  validateNoDuplicates(carArray);
+  validateCarCount(carArray);
+
+  return carArray;
 };
