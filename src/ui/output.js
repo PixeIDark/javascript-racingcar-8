3c598da826f1bf8a01a3e2acdc5b80f35048386d
PixeIDark <kiki7951@naver.com>
2025-10-27 04:12:59 +0900

    feat(output): 자동차 경주 출력 함수 추가
    - printRaceRound: 포맷된 라운드 정보를 콘솔에 출력
    - printRaceResult: 전체 경주 결과와 우승자 출력

---
+++ src/ui/output.js
@@ -0,0 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {OUTPUT_MESSAGES} from "../constants/outputMessages.js";
+
+const printEmptyLine = () => {
+  MissionUtils.Console.print("");
+};
+
+export const printRaceRound = (formattedRound) => {
+  for (const [car, distanceBar] of formattedRound) {
+    MissionUtils.Console.print(`${car} : ${distanceBar}`);
+  }
+
+  printEmptyLine();
+};
+
+export const printRaceResult = (winnersString, dashRaceHistory) => {
+  MissionUtils.Console.print(OUTPUT_MESSAGES.RACE_RESULT());
+
+  dashRaceHistory.forEach(printRaceRound);
+
+  MissionUtils.Console.print(OUTPUT_MESSAGES.WINNER(winnersString));
+};
