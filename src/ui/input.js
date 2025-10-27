4f3683d2b73c9353ab519a6e7bfc6ceef584c9ac
PixeIDark <kiki7951@naver.com>
2025-10-27 23:50:39 +0900

    refactor: readCarNames, readRounds 최적화 불필요한 await 제거

--- src/ui/input.js
+++ src/ui/input.js
@@ -1,10 +1,10 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import {MissionUtils} from "@woowacourse/mission-utils";
 import {PROMPT_MESSAGES} from "../constants/promptMessages.js";
 
 export const readCarNames = async () => {
-  return await MissionUtils.Console.readLineAsync(PROMPT_MESSAGES.CAR_NAMES);
+  return MissionUtils.Console.readLineAsync(PROMPT_MESSAGES.CAR_NAMES);
 };
 
 export const readRounds = async () => {
-  return await MissionUtils.Console.readLineAsync(PROMPT_MESSAGES.ROUNDS);
+  return MissionUtils.Console.readLineAsync(PROMPT_MESSAGES.ROUNDS);
 };
